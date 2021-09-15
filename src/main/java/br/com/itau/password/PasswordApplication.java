6025445d0d7cf1306c1d5012873a6cde9367df3e
EDGARD JOSEPH KIRIYAMA <edkiriyama@gmail.com>
2021-09-15 14:03:15 -0300

    atualizacao

--- src/main/java/br/com/itau/password/PasswordApplication.java
+++ src/main/java/br/com/itau/password/PasswordApplication.java
@@ -3,6 +3,7 @@ package br.com.itau.password;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class PasswordApplication {
 
