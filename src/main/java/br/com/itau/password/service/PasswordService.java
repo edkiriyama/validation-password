d7478ecd2c8ffff77442c076e55a6a77446ef46c
EDGARD JOSEPH KIRIYAMA <edkiriyama@gmail.com>
2021-09-17 11:37:54 -0300

    Update PasswordService.java

--- src/main/java/br/com/itau/password/service/PasswordService.java
+++ src/main/java/br/com/itau/password/service/PasswordService.java
@@ -8,7 +8,7 @@ import java.util.regex.Pattern;
 @Service
 public class PasswordService {
 
-    private static final String password_regex = "^(?=.*[A-Z])(?!.*(.).*\\1)(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%ˆ&*()-+]).{9,}$";
+    private static final String password_regex = "^(?=.*[A-Z])(?!.*(.).*\\1)(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%ˆ&*()-+])(?=\\S+$).{9,}$";
 
     public boolean validate(String password) {
         Pattern pattern = Pattern.compile(password_regex);
